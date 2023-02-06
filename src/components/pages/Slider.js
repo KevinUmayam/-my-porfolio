d45e2f8eb18d8091e1f582fdf99e08bd48df92c4
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-02-06 13:29:48 -0800

    adjusted image sizes to fit the slider length and width

--- src/components/pages/Slider.js
+++ src/components/pages/Slider.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import image1 from '../pages/assets/5-6-7-8P1.png'
+import image1 from '../pages/assets/5-6-7-8.jpeg'
 import image2 from '../pages/assets/AirDnD.jpeg'
-import image3 from '../pages/assets/WeatherApp-P3.jpeg'
-import image4 from '../pages/assets/fit-foodP2.jpeg'
-import image5 from '../pages/assets/teamgen.jpeg'
+import image3 from '../pages/assets/WeatherApp.jpeg'
+import image4 from '../pages/assets/fit-food.jpeg'
+import image5 from '../pages/assets/teamGen.jpeg'
 
 const projectSlider = () => {
     const slides = [{ src: { image1 } }, { src: { image2 } }, { src: { image3 } }, { src: { image4 } }, { src: { image5 } }]
@@ -11,7 +11,7 @@ const projectSlider = () => {
     return (
         <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
             <div
-                style={{ backgroundImage: `url(${image1})` }}
+                style={{ backgroundImage: `url(${image5})` }}
                 className='w-2/3 h-2/3 rounded-2xl bg-center bg-cover duration-500'
             ></div>
         </div>
