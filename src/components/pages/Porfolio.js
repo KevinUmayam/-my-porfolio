376eebfdff3a80cec78df03c06ab11cdccb6fad8
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-02-09 10:07:33 -0800

    adjusted styling for phone screen sizes

--- src/components/pages/Porfolio.js
+++ src/components/pages/Porfolio.js
@@ -3,7 +3,7 @@ import Slider from './Slider'
 
 export default function Portfolio() {
   return (
-    <div className="bg-black h-screen ">
+    <div className="bg-black h-[850px] sm:h-screen ">
       <div> <h1 className='flex justify-center text-center  rText text-4xl font-bold pb-8 pt-6 '>Project Display</h1></div>
       <div> <Slider /></div>
     </div>
