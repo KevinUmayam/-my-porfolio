5e0f609ad79f336687ff78a542f4e50d4c50714d
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-03-08 21:00:36 -0800

    added text

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -26,7 +26,7 @@ export default function AboutMe() {
       {/* phonescreen */}
       <div className="mt-10 mb-10 center-text">
         <h6 className="text-white text-xl text-center md:ml-32 ml- md:mr-32 ml-14 mr-14">
-          As a front-end web developer, I draw upon my background in sociology to stay abreast of the most desirable design and user experience trends on the web. I have successfully completed a full stack development certification from UCSD and have honed my skills in JavaScript, CSS, React.js, and responsive web design.
+          As a full stack developer, I draw upon my background in sociology to stay abreast of the most desirable design and user experience trends on the web. I have successfully completed a full stack development certification from UCSD and have honed my skills in JavaScript, CSS, React.js, and responsive web design.
         </h6>
       </div>
       <div className="border-custom-color border-8 border-double rounded-full p-2 pl-6 pr-6 mb-14">
